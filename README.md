281f2828855833715dd2dddd82dad766aee9a2db
tieje <toj320@gmail.com>
2024-02-01 20:15:11 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # websocket_server_sample_api_axum
 Practicing creating a websocket API server built with Axum.
+
+## Usage
+
+## Resources
+- https://docs.rs/axum/latest/axum/extract/ws/index.html
+
+## TODO
+
+- [ ] make a blog post about websockets in Axum Rust
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use axum::{extract::ws::WebSocketUpgrade, response::Response, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(get_ping))
+        .route("/ws", get(ws_handler));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade()
+}
+
+
+
+async fn get_ping() -> &'static str {
+    "pong!"
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::{routing::get, Router};
+    use axum_test::TestServer;
+
+    use crate::get_ping;
+
+    #[tokio::test]
+    async fn it_should_get() {
+        let ping = "/ping";
+        // Build an application with a route.
+        let app = Router::new().route(ping, get(get_ping));
+        // Run the application for testing.
+        let server = TestServer::new(app).unwrap();
+        // Get the request.
+        let response = server.get(ping).await;
+        assert_eq!(response.text(), "pong!");
+    }
+}
